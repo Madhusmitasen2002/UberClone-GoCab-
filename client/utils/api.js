25dffeb57d5883aae74aec4a261113c69e0978df
Madhusmita Sen <madhusmitasen27@gmail.com>
2025-09-18 23:22:51 +0530

    minor fix2

--- client/utils/api.js
+++ client/utils/api.js
@@ -1,5 +1,5 @@
 // client/utils/api.js
-const API_BASE_URL = "https://uberclone-gocab.onrender.com/";
+const API_BASE_URL = "https://uberclone-gocab.onrender.com";
 
 async function request(endpoint, options = {}) {
   try {
